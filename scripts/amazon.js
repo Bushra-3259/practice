50f91b6562a6912c2ced7e5cf3e5db975d32dab6
Bushra-3259 <bushraharun456@gmail.com>
2025-02-15 12:10:40 +0600

    Move addToCart into cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -58,32 +58,20 @@ let productsHTML = '';
     });
   
     document.querySelector('.js-products-grid').innerHTML = productsHTML;
+    
+    function updateCartQuantity() {
+      let cartQuantity = 0;
+
+        cart.forEach((cartItem) => {
+          cartQuantity += cartItem.quantity;
+    });
 
     document.querySelectorAll('.js-add-to-cart').forEach(( button) => {
       button.addEventListener('click', () => {
         const productId = button.dataset.productId;
-
-        let matchingItem;
-        cart.forEach((item) => {
-          if(productId === item.productId) {
-            matchingItem = item;
-          }
-        });
-
-        if(matchingItem) {
-          matchingItem.quantity += 1;
-        }
-        else {
-          cart.push({
-            productId: productId,
-            quantity: 1
-          });
-        }
-
-        let cartQuantity = 0;
-
-        cart.forEach((item) => {
-          cartQuantity += item.quantity;
+        addToCart(productId);
+        updateCartQuantity();
+        
         });
 
         document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
